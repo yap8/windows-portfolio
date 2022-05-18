97cf6a15cb515af8c071d8e88b42ac011312b7be
yap8 <nebozhenko.arseny@gmail.com>
2022-05-18 21:21:39 +0300

    added Time

--- src/components/Taskbar/Time.tsx
+++ src/components/Taskbar/Time.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+import date from 'date-and-time';
+
 import Button from '../Button';
 
 const Time: React.FC = () => {
-  return <Button active={true}>10:38 AM</Button>;
+  const [time, setTime] = useState(date.format(new Date(), 'hh:mm A'));
+
+  useEffect(() => {
+    setInterval(() => {
+      const now = new Date();
+
+      setTime(date.format(now, 'hh:mm A'));
+    }, 1000);
+  }, []);
+
+  return (
+    <Button className="cursor-default" active={true}>
+      {time}
+    </Button>
+  );
 };
 
 export default Time;
