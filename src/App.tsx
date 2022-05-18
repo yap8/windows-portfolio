1a98755a1f55b72e4771a698677a58f8562206e2
yap8 <nebozhenko.arseny@gmail.com>
2022-05-18 15:59:05 +0300

    moved Program logic into Programs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { observer } from 'mobx-react-lite';
-
 import Desktop from './components/Desktop';
-import { Program } from './components/Programs';
+import Programs from './components/Programs';
 import Taskbar from './components/Taskbar';
-import programs from './store/programs';
 
-const App: React.FC = observer(() => {
-  const handleClose = (id: number) => {
-    programs.closeProgram(id);
-  };
-
-  const handleActive = (id: number) => {
-    programs.makeProgramActive(id);
-  };
-
+const App: React.FC = () => {
   return (
     <div className="overflow-hidden relative">
       <Taskbar />
-      {programs.programs.map((program) => (
-        <Program
-          program={program}
-          handleClose={handleClose}
-          handleActive={handleActive}
-          key={program.id}
-        />
-      ))}
+      <Programs />
       <Desktop />
     </div>
   );
-});
+};
 
 export default App;
--- src/components/Programs/index.tsx
+++ src/components/Programs/index.tsx
@@ -1,8 +1,29 @@
+import { observer } from 'mobx-react-lite';
+import programs from '../../store/programs';
 import Program from './Program';
 
-const Programs = () => {
-  return <div>Programs</div>;
-};
+const Programs = observer(() => {
+  const handleClose = (id: number) => {
+    programs.closeProgram(id);
+  };
+
+  const handleActive = (id: number) => {
+    programs.makeProgramActive(id);
+  };
+
+  return (
+    <div>
+      {programs.programs.map((program) => (
+        <Program
+          program={program}
+          handleClose={handleClose}
+          handleActive={handleActive}
+          key={program.id}
+        />
+      ))}
+    </div>
+  );
+});
 
 export { Program };
 export default Programs;
