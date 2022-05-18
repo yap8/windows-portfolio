83cc3efed56e772420e669f2827b571a642b75c8
yap8 <nebozhenko.arseny@gmail.com>
2022-05-18 16:29:59 +0300

    made taskbar react to state

--- src/components/Taskbar/Window.tsx
+++ src/components/Taskbar/Window.tsx
@@ -1,14 +1,19 @@
-import { WindowI } from './Windows';
+import { Program } from '../../store/programs';
 import Button from '../Button';
 
 interface WindowProps {
-  window: WindowI;
+  window: Program;
+  handleActive: (id: number) => void;
 }
 
-const Window: React.FC<WindowProps> = ({ window }) => {
+const Window: React.FC<WindowProps> = ({ window, handleActive }) => {
   return (
     <li key={window.id}>
-      <Button className="mr-1 w-40" active={window.active}>
+      <Button
+        className="mr-1 w-40"
+        active={window.active}
+        onClick={() => handleActive(window.id)}
+      >
         {window.title}
       </Button>
     </li>
--- src/components/Taskbar/Windows.tsx
+++ src/components/Taskbar/Windows.tsx
@@ -1,29 +1,25 @@
+import { observer } from 'mobx-react-lite';
 import { twMerge } from 'tailwind-merge';
+
+import programs from '../../store/programs';
 import Window from './Window';
-
-export interface WindowI {
-  id: number;
-  title: string;
-  active: boolean;
-}
-
-const windows: WindowI[] = [
-  { id: 1, title: 'Program 1', active: true },
-  { id: 2, title: 'Program 2', active: false },
-];
 
 interface WindowProps {
   className?: string;
 }
 
-const Windows: React.FC<WindowProps> = ({ className }) => {
+const Windows: React.FC<WindowProps> = observer(({ className }) => {
+  const handleActive = (id: number) => {
+    programs.makeProgramActive(id);
+  };
+
   return (
     <ul className={twMerge('flex items-center', className)}>
-      {windows.map((window) => (
-        <Window window={window} key={window.id} />
+      {programs.programs.map((window) => (
+        <Window window={window} handleActive={handleActive} key={window.id} />
       ))}
     </ul>
   );
-};
+});
 
 export default Windows;
