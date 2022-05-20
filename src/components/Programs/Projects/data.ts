76c470a05e7803aa025271f37425b480b65bb68b
yap8 <nebozhenko.arseny@gmail.com>
2022-05-20 22:16:56 +0300

    changed projects icons

--- src/components/Programs/Projects/data.ts
+++ src/components/Programs/Projects/data.ts
@@ -3,17 +3,17 @@ import { ShortcutI } from '../../Shortcut';
 export const shortcuts: Array<ShortcutI> = [
   {
     title: 'Portfolio',
-    iconURL: 'images/Computer Input.ico',
+    iconURL: 'images/Help page.ico',
     program: 'contact',
   },
   {
     title: 'Flashcards',
-    iconURL: 'images/Book.ico',
+    iconURL: 'images/Help page.ico',
     program: 'contact',
   },
   {
     title: 'Messenger',
-    iconURL: 'images/Chat.ico',
+    iconURL: 'images/Help page.ico',
     program: 'contact',
   },
 ];
