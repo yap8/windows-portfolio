79d9b40e5f837ec69bdf9afc8e82623e3446c174
yap8 <nebozhenko.arseny@gmail.com>
2022-05-23 10:16:51 +0300

    temporarily removed language shortcut

--- src/components/Desktop/data.ts
+++ src/components/Desktop/data.ts
@@ -1,44 +1,44 @@
-import { ShortcutI } from '../Shortcut';
+import { ShortcutI } from "../Shortcut";
 
 export const shortcuts: Array<ShortcutI> = [
   {
-    title: 'Projects',
-    iconURL: 'images/Folder.ico',
-    program: 'projects',
+    title: "Projects",
+    iconURL: "images/Folder.ico",
+    program: "projects",
   },
-  ...Array(12).fill({ slug: Math.random() }),
+  ...Array(13).fill({ slug: Math.random() }),
+  // {
+  //   title: 'Language',
+  //   iconURL: 'images/Earth (16 colors).ico',
+  //   program: 'language',
+  // },
   {
-    title: 'Language',
-    iconURL: 'images/Earth (16 colors).ico',
-    program: 'language',
-  },
-  {
-    title: 'About me',
-    iconURL: 'images/User documents.ico',
-    program: 'about',
+    title: "About me",
+    iconURL: "images/User documents.ico",
+    program: "about",
   },
   ...Array(13).fill({ slug: Math.random() }),
   {
-    title: 'Resume',
-    iconURL: 'images/Notepad document.ico',
-    link: 'https://yap8.github.io/portfolio/Nebozhenko_Frontend_Resume.pdf',
+    title: "Resume",
+    iconURL: "images/Notepad document.ico",
+    link: "https://yap8.github.io/portfolio/Nebozhenko_Frontend_Resume.pdf",
   },
   ...Array(13).fill({ slug: Math.random() }),
   {
-    title: 'GitHub',
-    iconURL: 'images/User with computer.ico',
-    link: 'https://github.com/yap8',
+    title: "GitHub",
+    iconURL: "images/User with computer.ico",
+    link: "https://github.com/yap8",
   },
   ...Array(13).fill({ slug: Math.random() }),
   {
-    title: 'Contact',
-    iconURL: 'images/Mail.ico',
-    program: 'contact',
+    title: "Contact",
+    iconURL: "images/Mail.ico",
+    program: "contact",
   },
   ...Array(13).fill({ slug: Math.random() }),
   {
-    title: 'Old portfolio',
-    iconURL: 'images/Recycle Bin with paper.ico',
-    link: 'https://yap8.github.io/portfolio/',
+    title: "Old portfolio",
+    iconURL: "images/Recycle Bin with paper.ico",
+    link: "https://yap8.github.io/portfolio/",
   },
 ];
